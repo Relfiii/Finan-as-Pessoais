0ab8e72bf545ba05f87ede397b7f2626ca81eb22
GitHub Action <action@github.com>
2025-07-15 23:47:04 +0000

    Update docs folder with Flutter web build

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -274,7 +274,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e8453f24f2836d63153e78c33ab8893f
 "favicon-32x32.png": "192aa0bdf7b6963e3b57fd18ddea8b48",
 "version.json": "fdd69a42405d391754255882c4b575eb",
 "manifest.json": "41b34095339a7adf23150158452605f2",
-"flutter_bootstrap.js": "7863250fb2e085f42174b74e1e4c035a",
+"flutter_bootstrap.js": "f5cff3296a29269985eaaa18233f0dba",
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "canvaskit/canvaskit.wasm": "1f237a213d7370cf95f443d896176460",
@@ -286,8 +286,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "e8453f24f2836d63153e78c33ab8893f
 "canvaskit/canvaskit.js": "5fda3f1af7d6433d53b24083e2219fa0",
 "canvaskit/canvaskit.js.symbols": "48c83a2ce573d9692e8d970e288d75f7",
 "favicon-16x16.png": "ef1703cc607db88cc58885aeccb61e4b",
-"index.html": "c92406b192c93ac16a54c5500eca9d44",
-"/": "c92406b192c93ac16a54c5500eca9d44",
+"index.html": "c2a41bc95c0b9f34b5d7e2ad165ac85e",
+"/": "c2a41bc95c0b9f34b5d7e2ad165ac85e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "favicon.ico": "192aa0bdf7b6963e3b57fd18ddea8b48",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
@@ -297,7 +297,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e8453f24f2836d63153e78c33ab8893f
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "index_backup.html": "26dcd437a8f79290c7337145a16f7696",
-"main.dart.js": "28dc1a26ce0ca504c74c38027bba3d5c"};
+"main.dart.js": "d590eec2d3f2c421f881d75b48d5fb48"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
